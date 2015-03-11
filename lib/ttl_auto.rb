c53f0bafafb43b291b6c0d901996b5db434ae886
hiroshi shimoda <hilolih@gmail.com>
2015-03-11 11:54:11 +0900

    add macro.rb

--- lib/ttl_auto.rb
+++ lib/ttl_auto.rb
@@ -1,5 +1,8 @@
 require "ttl_auto/version"
 require "ttl_auto/macro"
+require 'pry'
+require 'pry-doc'
+require 'pry-byebug'
 
 
 module TtlAuto
---
+++ lib/ttl_auto/macro.rb
@@ -0,0 +1,20 @@
+# -*- encoding: utf-8 -*-
+module TtlAuto
+  class Macro
+    #include Constant
+    #include TtlAuto::Log
+    attr_reader :yaml, :command_templates
+    attr_accessor :definitions, :command_macros, :rootdir, :private_keydir, :inidir
+    
+    def initialize &block
+      @rootdir = "./out"
+      @inidir  = "ini"
+      instance_eval(&block) if block_given?
+    end
+
+    def out!
+      
+binding.pry
+    end
+
+end
