305c37a5c5640a2057e286fce3a3868f24814035
hilolih <hilolih@gmail.com>
2017-02-12 21:34:24 +0900

    @record = recordの意味のない変数を削除

--- lib/ttlauto/template.rb
+++ lib/ttlauto/template.rb
@@ -1,4 +1,10 @@
 # -*- encoding: utf-8 -*-
+#
+# TtlAuto::Template
+#
+# テンプレートマクロを生成する
+# Ttl, Expectのスーパークラス
+#
 require 'erb'
 module TtlAuto
     class Template
@@ -6,15 +12,16 @@ module TtlAuto
         include TtlAuto::Log
         def initialize record, category, cmd_templates={}
             @path = "#{OUT}/#{category}".downcase
-            @record = record
+            #@record = record
             @category = category
             @cmd_templates = cmd_templates
+            #@rd = TtlAuto::Record.new(record)
+            @rd = TtlAuto::Record.new(record)
         end
 
         def timestamp
             Time.now.strftime('%Y/%m/%d %H:%M:%S')
         end
-
 
     end
 end
--- lib/ttlauto/template/ttl.rb
+++ lib/ttlauto/template/ttl.rb
@@ -5,31 +5,34 @@
 # erbを使って、ttlマクロを生成する
 #
 module TtlAuto
-  class Ttl < Template
-    attr_reader :path, :record
+    class Ttl < Template
+        attr_reader :path, :record
 
-    def bind
-      rd = TtlAuto::Record.new(@record)
-      log_debug  "#{@path}/#{record['name']}.ttl"
-      unless @cmd_templates.empty?
-        rd.replace_macro @cmd_templates
-      end
-      # set valid public keyfile path
-      if rd.keyfile?
-        rd.set_keyfile_path @category
-      end
-      # set valid inifile path
-      if rd.inifile?
-        rd.set_inifile_path @category
-      end
-      #
-      record = rd.body
-      description = rd.description
-      FileUtils.mkdir_p @path unless File.exists?(@path)
-      open( "#{@path}/#{record['name'].downcase}.ttl", "w:windows-31j"){|f|
-        f.puts ERB.new(IO.read(TEMPLATE), nil, '-').result(binding)
-      }
+        def bind
+            unless @cmd_templates.empty?
+                @rd.replace_macro @cmd_templates
+            end
+            # set valid public keyfile path
+            if @rd.keyfile?
+                @rd.set_keyfile_path @category
+            end
+            # set valid inifile path
+            if @rd.inifile?
+                @rd.set_inifile_path @category
+            end
+            #
+            record = @rd.body
+            description = @rd.description
+            #
+            # ファイル書き出し処理
+            # description や recordの変数をbindingするため
+            # あえて同じメソッドに。
+            #
+            FileUtils.mkdir_p @path unless File.exists?(@path)
+            open( "#{@path}/#{record['name'].downcase}.ttl", "w:windows-31j"){|f|
+                f.puts ERB.new(IO.read(TEMPLATE), nil, '-').result(binding)
+            }
+        end
+
     end
-
-  end
 end
