d7b5a8cf854a9b69988bae722e65a36f59e365cb
hilolih <hilolih@gmail.com>
2017-02-12 21:11:31 +0900

    ini timestampメソッドをTtlからTemplateへ移動

--- lib/ttlauto/template.rb
+++ lib/ttlauto/template.rb
@@ -2,6 +2,18 @@ module TtlAuto
     class Template
         include Constant
         include TtlAuto::Log
+        def initialize record, category, cmd_templates={}
+            @path = "#{OUT}/#{category}".downcase
+            @record = record
+            @category = category
+            @cmd_templates = cmd_templates
+        end
+
+        def timestamp
+            Time.now.strftime('%Y/%m/%d %H:%M:%S')
+        end
+
+
     end
 end
 
